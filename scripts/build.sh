06bfaeb0f2633d55f654628bea9c8c57f723c5f4
Kristoffel Pirard <kristoffel.pirard@midiagnostics.com>
2024-05-09 11:14:28 +0200

    (1/n) add open_spiel as a dependency
    * add required dependencies
    * we need cmake > 3.17 => choose ubuntu 22 as base image

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,11 +12,42 @@ else
     BUILD_IMAGE=$XPYBIND_BUILD_IMAGE
 fi
 
+
+function get_open_spiel() {
+
+    OPEN_SPIEL_DIR="$PROJECT_ROOT/deps/open_spiel"
+
+    [ -d $OPEN_SPIEL_DIR ] || git clone --depth 1 --branch v1.4 https://github.com/google-deepmind/open_spiel "$OPEN_SPIEL_DIR"
+    (
+        cd "$OPEN_SPIEL_DIR/open_spiel"
+        [ -d build ] || mkdir build
+    )
+
+    docker run --rm \
+        --user $(id -u):$(id -g) \
+        --volume "$PROJECT_ROOT":/src \
+        --workdir /src/deps/open_spiel \
+        $BUILD_IMAGE \
+        ./install.sh
+
+    docker run --rm \
+        --user $(id -u):$(id -g) \
+        --volume "$PROJECT_ROOT":/src \
+        --workdir /src/deps/open_spiel \
+        $BUILD_IMAGE \
+        open_spiel/scripts/build_and_run_tests.sh
+}
+
 function configure() {
 
     docker build -t $BUILD_IMAGE dev-image
 
-    mkdir build
+    [ -d build ] || mkdir build
+    [ -d deps ] || mkdir deps
+
+    get_open_spiel
+
+
 
     docker run --rm \
         --user $(id -u):$(id -g) \
